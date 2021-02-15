faff79aadda22031f9656816c7053d177fb3f72f
ifeslopes <ifes.lopes@gmail.com>
2021-02-15 20:02:19 -0300

    Department entity class

---
+++ src/aplication/Program.java
@@ -0,0 +1,11 @@
+package aplication;
+
+import model.entities.Department;
+
+public class Program {
+    public static void main(String[] args) {
+        Department obj = new Department(1,"Bokk");
+        System.out.println(obj);
+
+    }
+}
