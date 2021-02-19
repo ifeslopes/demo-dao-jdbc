8a1e2f9f9bad5f6c3aa8e1a51ff939f6ceae3e1d
ifeslopes <ifes.lopes@gmail.com>
2021-02-19 19:52:42 -0300

    Delete implememtation

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -7,10 +7,13 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 
 public class Program {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
         SellerDao sellerDao = DaoFactory.createSellerDao();
         Seller seller = sellerDao.findById(2);
 
@@ -44,5 +47,12 @@ public class Program {
         seller.setName("Martha Waine");
         sellerDao.update(seller);
         System.out.println("Update completed");
+
+        System.out.println("\n==== TEST 5: seller update =====");
+        System.out.println("Enter id for delete test: ");
+        int  id =sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed: ");
+        sc.close();
     }
 }
